b07591d7f18f479c4c55f27ad9243ea591870d1a
Joshua Jones <joshuajones02@live.com>
2021-01-28 00:52:28 -0500

    Completed "Configuration" smoke tests

--- TMDb.Client/API/V3/Models/Configuration/PrimaryTranslationsConfigurationResponse.cs
+++ TMDb.Client/API/V3/Models/Configuration/PrimaryTranslationsConfigurationResponse.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace TMDb.Client.Api.V3.Models.Configuration
 {
     public class PrimaryTranslationsConfigurationResponse : TMDbResponse
     {
+        [JsonProperty("results")]
         public virtual IEnumerable<string> LanguageCodeDashRegionCode { get; set; }
     }
 }
